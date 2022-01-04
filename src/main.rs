e0c0f0d56e34a2db0ad5640ba0d85296a77577ff
Adrian Szczechura <a.szczechura.98@gmail.com>
2022-01-04 01:35:51 +0100

    feat: add basic login hanlder with string typing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::form::Form;
+
+#[derive(FromForm)]
+struct LoginInput<'r> {
+    email: &'r str,
+    password: &'r str
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn hello() -> &'static str {
+    "Hello wrld"
+}
+
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello_with_params(name: &str, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+#[post("/login", data = "<input>")]
+fn login(input: Form<LoginInput<'_>>) -> &'static str {
+    "Login"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/api", routes![hello, hello_with_params, login])
 }
